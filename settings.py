652b557abbc1fc9f7328a7a81063668dec61ec78
Andreas Johnsen <dr3as@dr3as.net>
2024-03-09 15:53:09 +0100

    first commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+easy_wood_start = "100";
+easy_rock_start = "100";
+easy_people_start = "";
+
+medium_wood_start = "100";
+medium_rock_start = "100";
+medium_people_start = "";
+
+hard_wood_start = "100";
+hard_rock_start = "100";
+hard_people_start = "";
---
+++ start.py
@@ -0,0 +1,21 @@
+import os
+from settings.py import *
+
+print("Welcome to Console City");
+print("Choose between a new game or to load a save");
+print("1. New Game");
+print("2. Load Game");
+first_menu = input("");
+def clear_console():
+    os.system('clear')
+
+clear_console();
+print("Choose difficulty");
+print("1. Easy");
+print("2. Medium");
+print("3. Hard");
+difficulty = input("");
+def clear_console():
+    os.system('clear')
+
+clear_console();
